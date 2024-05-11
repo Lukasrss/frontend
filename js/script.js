bee852bae495b9f60c277bd6926aa19ad43988be
Lucas Roberto <107315455+Lukasrss@users.noreply.github.com>
2024-05-10 21:17:51 -0300

    updata add script alterar cor do site

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-//script
+// inicio Função de alterar o tema do site
+const toggleTheme = document.getElementById("toggleTheme");
+const rootHtml = document.documentElement; //para pegar a raiz do projeto.
+const accordionHeaders = document.querySelectorAll(".accordion-header");
+const menuLinks = document.querySelectorAll(".menu-link");
+
+function changeTheme(){
+    const currentTheme = rootHtml.getAttribute("data-theme");
+    //  teste === teste ? sim : não
+    currentTheme === "dark" ? rootHtml.setAttribute("data-theme","light") : rootHtml.setAttribute("data-theme","dark");
+    
+    toggleTheme.classList.toggle("bi-sun");
+    toggleTheme.classList.toggle("bi-moon-stars");
+}
+
+toggleTheme.addEventListener("click", changeTheme);
+// Fim da função
