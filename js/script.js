cb17899adeaaef7f32acfed18545e85d4896e7d5
Lucas Roberto <107315455+Lukasrss@users.noreply.github.com>
2024-05-14 20:01:27 -0300

    updata script completo

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-// inicio Função de alterar o tema do site
 const toggleTheme = document.getElementById("toggleTheme");
 const rootHtml = document.documentElement; //para pegar a raiz do projeto.
 const accordionHeaders = document.querySelectorAll(".accordion-header");
 const menuLinks = document.querySelectorAll(".menu-link");
 
+// inicio Função de alterar o tema do site
 function changeTheme(){
     const currentTheme = rootHtml.getAttribute("data-theme");
     //  teste === teste ? sim : não
@@ -15,3 +15,23 @@ function changeTheme(){
 
 toggleTheme.addEventListener("click", changeTheme);
 // Fim da função
+
+// Função acordion 
+accordionHeaders.forEach(header => {
+    header.addEventListener("click", () => {
+        const accordionItem = header.parentElement;
+        const accordionActive = accordionItem.classList.contains("active");
+
+        accordionActive ? accordionItem.classList.remove("active") : accordionItem.classList.add("active")
+    })
+})
+// Fim função accordion
+
+//Função munu links
+
+menuLinks.forEach(item => {
+    item.addEventListener("click", () => {
+        menuLinks.forEach(i => i.classList.remove("active"));
+        item.classList.add("active");
+    })
+})
